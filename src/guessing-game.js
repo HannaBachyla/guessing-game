0c2c112a93f7c200bbc719b67df90e55cc0228c7
HannaBachyla <annahodunova1@gmail.com>
2022-08-14 23:02:54 +0300

    init: ready task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+        this.min = min;
+        this.max = max;
+      }
+      guess() {
+        let a = Math.round(this.max / 2 - this.min / 2 + this.min);
+        return a;
+      }
+      lower() {
+        this.max = Math.round(this.max / 2 - this.min / 2 + this.min);
+        this.guess()
+      }
+      greater() {
+        this.min = Math.round(this.max / 2 - this.min / 2 + this.min);
+        this.guess()
+      }
 }
 
 module.exports = GuessingGame;
